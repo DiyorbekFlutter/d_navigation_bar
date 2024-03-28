ce2c8b70b592ed072ab75bdbba04cd99637fdff2
Diyorbek4926 <131335654+Diyorbek4926@users.noreply.github.com>
2024-03-28 10:28:44 +0500

    DNavigationBar muvofaqiyetli bajarildi

--- lib/d_navigation_bar.dart
+++ lib/d_navigation_bar.dart
@@ -1,43 +1,63 @@
 import 'package:flutter/material.dart';
 
+/// default controller
+DNavigationBarController _defaultController = DNavigationBarController();
+
+// ignore: must_be_immutable
 class DNavigationBar extends StatelessWidget {
   final List<DNavigationBarItem> items;
   final void Function(int index)? onChanged;
+  final MainAxisAlignment? mainAxisAlignment;
+  final Color backgroundColor;
+  final bool useShadow;
+  final EdgeInsetsGeometry? padding;
+  final BorderRadiusGeometry borderRadius;
+  final double height;
+  final Decoration? decoration;
   DNavigationBarController? controller;
 
   DNavigationBar({
     super.key,
     required this.items,
     this.controller,
-    this.onChanged
+    this.onChanged,
+    this.mainAxisAlignment,
+    this.backgroundColor = Colors.white,
+    this.useShadow = true,
+    this.padding,
+    this.borderRadius = BorderRadius.zero,
+    this.height = 60.0,
+    this.decoration
   }){
     controller ??= _defaultController;
     for(int i=0; i<items.length; i++){
       items[i]._controller = controller!;
       items[i]._index = i;
-      items[i].onChanged = onChanged;
+      items[i]._onChanged = onChanged;
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return BottomAppBar(
-      height: 60,
+      height: height,
       color: Colors.transparent,
       elevation: 0,
-      padding: const EdgeInsets.all(0),
+      padding: padding ?? const EdgeInsets.all(0),
       child: Container(
-        decoration: const BoxDecoration(
-            boxShadow: [
+        decoration: decoration ?? BoxDecoration(
+            color: backgroundColor,
+            borderRadius: borderRadius,
+            boxShadow: useShadow ? const [
               BoxShadow(
                 color: Colors.grey,
                 blurStyle: BlurStyle.outer,
                 blurRadius: 2,
               )
-            ]
+            ] : null
         ),
         child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisAlignment: mainAxisAlignment ?? MainAxisAlignment.spaceEvenly,
           children: items,
         ),
       ),
@@ -46,55 +66,90 @@ class DNavigationBar extends StatelessWidget {
 }
 
 
-
+// ignore: must_be_immutable
 class DNavigationBarItem extends StatefulWidget  {
   final Widget icon;
-  final String label;
-  final Widget? selectedIcon;
+  final String? label;
+  final Widget? activeIcon;
+  final TextStyle labelStyle;
+  final TextStyle activeLabelStyle;
+  final double iconLabelSpacing;
+  final Color highlightColor;
+  final void Function()? onPressed;
 
-  void Function(int index)? onChanged;
+  void Function(int index)? _onChanged;
   late DNavigationBarController _controller;
   int _index = 0;
 
   DNavigationBarItem({
     super.key,
     required this.icon,
-    required this.label,
-    this.selectedIcon
+    this.label,
+    this.activeIcon,
+    this.labelStyle = const TextStyle(fontSize: 10),
+    this.activeLabelStyle = const TextStyle(fontSize: 10),
+    this.iconLabelSpacing = 3.0,
+    this.highlightColor = Colors.transparent,
+    this.onPressed
   });
 
   @override
   State<DNavigationBarItem> createState() => _DNavigationBarItemState();
 }
+
 class _DNavigationBarItemState extends State<DNavigationBarItem> {
   @override
   Widget build(BuildContext context) {
     return IconButton(
       onPressed: (){
         widget._controller.currentIndex = widget._index;
-        if(widget.onChanged != null) widget.onChanged!(widget._controller.currentIndex);
+        if(widget._onChanged != null) widget._onChanged!(widget._controller.currentIndex);
+        if(widget.onPressed != null) widget.onPressed!();
         setState(() {});
       },
-      highlightColor: Colors.transparent,
+      highlightColor: widget.highlightColor,
       icon: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          widget._controller.currentIndex == widget._index && widget.selectedIcon != null
-              ? widget.selectedIcon! : widget.icon,
-          const SizedBox(height: 3),
-          Text(widget.label, style: const TextStyle(fontSize: 10))
+          widget._controller.currentIndex == widget._index && widget.activeIcon != null
+              ? widget.activeIcon! : widget.icon,
+          if(widget.label != null) SizedBox(height: widget.iconLabelSpacing),
+          if(widget.label != null)
+            Text(
+                widget.label!,
+                style: widget._controller.currentIndex == widget._index ? widget.activeLabelStyle : widget.labelStyle
+            )
         ],
       ),
     );
   }
 }
 
-DNavigationBarController _defaultController = DNavigationBarController();
-
+/// DNavigationBarController
 class DNavigationBarController {
-  int currentIndex = 0;
-  List<Widget> pages = [];
-  List<PreferredSizeWidget> appBars = [];
-  Widget get page => pages[currentIndex];
-  PreferredSizeWidget get appBar => appBars[currentIndex];
+  int currentIndex;
+  List<Widget> pages;
+  List<PreferredSizeWidget> appBars;
+
+  DNavigationBarController({
+    this.currentIndex = 0,
+    this.pages = const [],
+    this.appBars = const []
+  });
+
+  Widget get page {
+    try{
+      return pages[currentIndex];
+    } catch(e){
+      return Center(child: Text(e.toString()));
+    }
+  }
+
+  PreferredSizeWidget get appBar {
+    try{
+      return appBars[currentIndex];
+    } catch(e){
+      return AppBar(title: Text(e.toString()), backgroundColor: Colors.red);
+    }
+  }
 }
